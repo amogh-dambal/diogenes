17040a92d4f78528510e3153484a5534cc222395
Amogh Dambal <amdam2013@gmail.com>
2020-07-19 00:48:43 -0500

    update move tests

--- test/tests-move.cpp
+++ test/tests-move.cpp
@@ -23,9 +23,10 @@ TEST_CASE("testing simple move constructor with e4", "move()")
     REQUIRE(!m.is_castle());
     REQUIRE(!m.is_ep());
     REQUIRE(!m.is_promotion());
-
+    REQUIRE(m.to_string() == "e4");
 }
 
+// capture testing
 TEST_CASE("testing simple move constructor with Nxc3")
 {
     Board::Square from = Board::Square::b1;
@@ -40,6 +41,7 @@ TEST_CASE("testing simple move constructor with Nxc3")
     REQUIRE(!m.is_castle());
     REQUIRE(!m.is_ep());
     REQUIRE(!m.is_promotion());
+    REQUIRE(m.to_string() == "Nxc3");
 }
 
 TEST_CASE("testing simple move constructor with castling")
@@ -57,4 +59,29 @@ TEST_CASE("testing simple move constructor with castling")
     REQUIRE(m.is_castle());
     REQUIRE(!m.is_ep());
     REQUIRE(!m.is_promotion());
+    REQUIRE(m.to_string() == "O-O");
+}
+
+TEST_CASE("testing simple move constructor with promotion")
+{
+    U64 flags = Move::KNIGHT_PROMOTION;
+    move e8(Board::Square::e7, Board::Square::e8, Move::PieceEncoding::PAWN, flags);
+
+    REQUIRE(e8.is_promotion());
+    REQUIRE(!e8.is_double_push());
+    REQUIRE(!e8.is_capture());
+    REQUIRE(!e8.is_castle());
+    REQUIRE(e8.to_string() == "e8=N");
+}
+
+TEST_CASE("testing move constructor with promotion x capture")
+{
+    U64 flags = Move::BISHOP_PROMOTION | Move::CAPTURE_FLAG;
+    move exd1(Board::Square::e2, Board::Square::d1, Move::PieceEncoding::PAWN, flags);
+
+    REQUIRE(exd1.is_promotion());
+    REQUIRE(!exd1.is_castle());
+    REQUIRE(exd1.is_capture());
+    REQUIRE(!exd1.is_double_push());
+    REQUIRE(exd1.to_string() == "exd1=B");
 }
