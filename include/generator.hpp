21651cfec44d1cbe1dc60a71d63d26a74e6d0888
Amogh Dambal <amdam2013@gmail.com>
2020-07-21 00:27:02 -0500

    refactor attack generation

--- include/generator.hpp
+++ include/generator.hpp
@@ -10,6 +10,7 @@
 #include "move.hpp"
 #include "board.hpp"
 #include "bitboard.hpp"
+#include "attacks.hpp"
 /**
  * move generator with
  * functions to generate
@@ -56,19 +57,7 @@ private:
     void generate_black_queen_moves();
     void generate_black_king_moves();
 
-    U64 generate_white_pawn_push_targets(bool single=true) const;
-    U64 generate_white_pawn_attacks() const;
-    U64 generate_white_knight_attacks() const;
-    U64 generate_white_bishop_attacks() const;
-    U64 generate_white_rook_attacks() const;
-    U64 generate_white_queen_attacks() const;
 
-    U64 generate_black_pawn_push_targets(bool single=true) const;
-    U64 generate_black_pawn_attacks() const;
-    U64 generate_black_knight_attacks() const;
-    U64 generate_black_bishop_attacks() const;
-    U64 generate_black_rook_attacks() const;
-    U64 generate_black_queen_attacks() const;
 
 };
 
