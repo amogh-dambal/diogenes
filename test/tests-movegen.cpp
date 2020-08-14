5210391b15cd57a73bb82cc29632f0f973586904
Amogh Dambal <amdam2013@gmail.com>
2020-08-14 01:43:18 -0500

    add pin ray generation verification tests

--- test/tests-movegen.cpp
+++ test/tests-movegen.cpp
@@ -10,6 +10,7 @@
 #include "stds.hpp"
 
 // TODO: write more comprehensive move-generation tests
+//  - perft testing
 
 /* PSEUDOLEGAL MOVE GENERATION */
 
@@ -149,24 +150,67 @@ TEST_CASE("random position - BLACK to move", "[movegen]")
     }
 }
 
-    SECTION("all moves, random position", "[movegen]")
+/* LEGAL MOVE GENERATION */
+
+// test pin rays
+TEST_CASE("pin ray generation")
+{
+    SECTION("white to move")
     {
-        std::cout << "================================================\n";
-        fen_record = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
-        b = new board(fen_record);
-        g = new generator(*b);
-
-        auto lmoves = g->get_legal_moves();
-
-        for (const move& m : lmoves)
+        SECTION("one pin")
         {
-            std::cout << m << " ";
+            std::string fen = "r3k2r/ppp2Npp/1b5n/4p2b/2B1P2q/BQP2P2/P5PP/RN5K w kq - 1 1";
+            board b(fen);
+            generator* g;
+
+            g = new generator(b);
+            auto mvs = g->_TEST_pin_rays();
+            delete g;
+            REQUIRE(mvs.size() == 1);
+
         }
-        std::cout << std::endl;
-
-        REQUIRE(lmoves.size() == 6);
-
-        delete b;
-        delete g;
     }
+    SECTION("black to move")
+    {
+        SECTION("no pin")
+        {
+            std::string fen = "r2n1rk1/1ppb2pp/1p1p4/3Ppq1n/2B3P1/2P4P/PP1N1P1K/R2Q1RN1 b - - 0 1";
+            board b(fen);
+            generator* gen = new generator(b);
+
+            auto mvs = gen->_TEST_pin_rays();
+
+            REQUIRE(mvs.size() == 0);
+
+            delete gen;
+
+            fen = "rnbq1rk1/pp3pbp/4pnp1/2p3B1/2B1P3/2P2N2/PP1N1PPP/R2QK2R b KQ - 1 8";
+            b = board(fen);
+            gen = new generator(b);
+
+            mvs = gen->_TEST_pin_rays();
+            REQUIRE(mvs.size() == 0);
+            delete gen;
+        }
+        SECTION("one pin")
+        {
+            std::string fen = "rnbq1rk1/pp2ppbp/5np1/2p3B1/2B1P3/1QP2N2/PP1N1PPP/R3K2R b KQ - 1 8";
+            board b(fen);
+            generator* g;
+
+            g = new generator(b);
+            auto mvs = g->_TEST_pin_rays();
+            REQUIRE(mvs.size() == 1);
+            delete g;
+
+            fen = "r3k2r/p1ppqpb1/bn2pnp1/1B1PN3/1p2P3/2N2Q1p/PPPB1PPP/R3K2R b KQkq a3 0 1";
+            b = board(fen);
+            g = new generator(b);
+            auto pins = g->_TEST_pin_rays();
+            REQUIRE(mvs.size() == 1);
+        }
+    }
+
 }
+
+/* LEGAL PIECEWISE MOVE GENERATION */
